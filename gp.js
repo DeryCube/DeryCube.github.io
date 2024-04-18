22d78ee10e1fea7af5a7d3894dbf2e93a8f652c9
BuildTools <unconfigured@null.spigotmc.org>
2024-04-18 03:21:40 +0200

    idk 2

--- gp.js
+++ gp.js
@@ -1,11 +1,11 @@
-$('body').mouseup(function(){
+$('body').mouseup(function () {
     $('.output').height($('textarea').height());
     $('.output').height($('textarea').height());
 });
 
-$.fn.selectRange = function(start, end){
-    if(!end) end = start;
-    return this.each(function(){
+$.fn.selectRange = function (start, end) {
+    if (!end) end = start;
+    return this.each(function () {
         if (this.setSelectionRange) {
             this.focus();
             this.setSelectionRange(start, end);
@@ -19,14 +19,14 @@ $.fn.selectRange = function(start, end){
     });
 };
 
-$('.bgs div').click(function(elem){
+$('.bgs div').click(function (elem) {
     $('.output').css('background', $(elem.target).css('background'));
-    $('.bgs div').css('border',0);
+    $('.bgs div').css('border', 0);
     $(elem.target).css('border', '3px #aaa solid');
 });
 
 
-$('.tools button').click(function(e){
+$('.tools button').click(function (e) {
     var caretPos = motd_raw[0].selectionStart;
     var textAreaTxt = motd_raw.val();
     var txtToAdd = '&' + $(this).attr('data-color');
@@ -39,67 +39,66 @@ $('.tools button').click(function(e){
 
 
 
-function colour (text) {
-left = htmlEncode("<");  
-right = htmlEncode(">");
-text = text + " &8| &fPseudo"
-text = text.replace(/</gi, left);  
-text = text.replace(/>/gi, right);
-text = text.replace(/\n/gi, "&r<br />");
-//colours
-text = text.replace(/&0/gi,'</span>&r<span class="c-1">');
-text = text.replace(/&1/gi,'</span>&r<span class="c-2">');
-text = text.replace(/&2/gi,'</span>&r<span class="c-3">');
-text = text.replace(/&3/gi,'</span>&r<span class="c-4">');
-text = text.replace(/&4/gi,'</span>&r<span class="c-5">');
-text = text.replace(/&5/gi,'</span>&r<span class="c-6">');
-text = text.replace(/&6/gi,'</span>&r<span class="c-7">');
-text = text.replace(/&7/gi,'</span>&r<span class="c-8">');
-text = text.replace(/&8/gi,'</span>&r<span class="c-9">');
-text = text.replace(/&9/gi,'</span>&r<span class="c-10">');
-text = text.replace(/&a/gi,'</span>&r<span class="c-11">');
-text = text.replace(/&b/gi,'</span>&r<span class="c-12">');
-text = text.replace(/&c/gi,'</span>&r<span class="c-13">');
-text = text.replace(/&d/gi,'</span>&r<span class="c-14">');
-text = text.replace(/&e/gi,'</span>&r<span class="c-15">');
-text = text.replace(/&f/gi,'</span>&r<span class="c-16">');
-//bold
-text = text.replace(/&l/gi,"<span style='font-weight:900;'>");
-//italic
-text = text.replace(/&o/gi,"<span style='font-style:italic;'>");
-//strikethrough
-text = text.replace(/&m/gi,"<span style='text-decoration:line-through'>");
-//underlined
-text = text.replace(/&n/gi,"<span style='text-decoration:underline'>");
-//obfuscated
-text = text.replace(/&k/gi,"<span class='obfuscated'>");
-//reset
-text = text.replace(/&r/gi, "</span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span>");
-                    
-document.getElementById('output').innerHTML=text;
+function colour(text) {
+    left = htmlEncode("<");
+    right = htmlEncode(">");
+    text = text + " &fPseudo"
+    text = text.replace(/</gi, left);
+    text = text.replace(/>/gi, right);
+    text = text.replace(/\n/gi, "&r<br />");
+    //colours
+    text = text.replace(/&0/gi, '</span>&r<span class="c-1">');
+    text = text.replace(/&1/gi, '</span>&r<span class="c-2">');
+    text = text.replace(/&2/gi, '</span>&r<span class="c-3">');
+    text = text.replace(/&3/gi, '</span>&r<span class="c-4">');
+    text = text.replace(/&4/gi, '</span>&r<span class="c-5">');
+    text = text.replace(/&5/gi, '</span>&r<span class="c-6">');
+    text = text.replace(/&6/gi, '</span>&r<span class="c-7">');
+    text = text.replace(/&7/gi, '</span>&r<span class="c-8">');
+    text = text.replace(/&8/gi, '</span>&r<span class="c-9">');
+    text = text.replace(/&9/gi, '</span>&r<span class="c-10">');
+    text = text.replace(/&a/gi, '</span>&r<span class="c-11">');
+    text = text.replace(/&b/gi, '</span>&r<span class="c-12">');
+    text = text.replace(/&c/gi, '</span>&r<span class="c-13">');
+    text = text.replace(/&d/gi, '</span>&r<span class="c-14">');
+    text = text.replace(/&e/gi, '</span>&r<span class="c-15">');
+    text = text.replace(/&f/gi, '</span>&r<span class="c-16">');
+    //bold
+    text = text.replace(/&l/gi, "<span style='font-weight:900;'>");
+    //italic
+    text = text.replace(/&o/gi, "<span style='font-style:italic;'>");
+    //strikethrough
+    text = text.replace(/&m/gi, "<span style='text-decoration:line-through'>");
+    //underlined
+    text = text.replace(/&n/gi, "<span style='text-decoration:underline'>");
+    //obfuscated
+    text = text.replace(/&k/gi, "<span class='obfuscated'>");
+    //reset
+    text = text.replace(/&r/gi, "</span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span></span>");
+
+    document.getElementById('output').innerHTML = text;
 
 }
 
 
-$(document).ready(function(){
-    $("textarea").keydown(function(e){
-    if (e.keyCode == 13 && !e.shiftKey)
-    {
-      // prevent default behavior
-      e.preventDefault();
-      //alert("ok");
-      return false;
-      }
+$(document).ready(function () {
+    $("textarea").keydown(function (e) {
+        if (e.keyCode == 13 && !e.shiftKey) {
+            // prevent default behavior
+            e.preventDefault();
+            //alert("ok");
+            return false;
+        }
     });
 });
 
-setInterval(function() {
-$('.obfuscated').text(randomizer($('.obfuscated').text()));
+setInterval(function () {
+    $('.obfuscated').text(randomizer($('.obfuscated').text()));
 }, 100);
 
 
-function htmlEncode(value){
-return $('<div/>').text(value).html();
+function htmlEncode(value) {
+    return $('<div/>').text(value).html();
 }
 
 function randomizer(rawr) {
@@ -107,7 +106,7 @@ function randomizer(rawr) {
     var text = '';
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < length; i++ )
+    for (var i = 0; i < length; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
